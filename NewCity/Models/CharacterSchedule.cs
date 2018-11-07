566c251b4b134b6e078b89058c09c838ce9ff0d8
firstbloodgiver <632940702@qq.com>
2018-11-08 00:43:21 +0800

    #3

---
+++ NewCity/Models/CharacterSchedule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewCity.Models
+{
+    public class CharacterSchedule
+    {
+
+        public Guid ID { get; set; }
+        public Guid CharacterID { get; set; }
+        public Guid StorySeriesID { get; set; }
+        public Guid StoryCardID { get; set; }
+
+        /// <summary>
+        /// 是否在主页面
+        /// </summary>
+        /// <returns></returns>
+        public bool IsMain { get; set; }
+
+
+    }
+}
---
+++ NewCity/Models/UserCharacter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewCity.Models
+{
+    public class UserCharacter
+    {
+        /// <summary>
+        /// 角色ID
+        /// </summary>
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// 角色名称 
+        /// </summary>
+        public string CharacterName { get; set; }
+
+        /// <summary>
+        /// 角色故事进度
+        /// </summary>
+        public ICollection<CharacterSchedule> CharacterSchedules { get; set; }
+
+    }
+}
