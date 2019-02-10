b874dc9ad9b65a271923ba715e0e9e1b71e2676c
firstbloodgiver <632940702@qq.com>
2019-02-11 00:20:42 +0800

    增加效果

--- NewCity/Enum/EnumConditionType.cs
+++ NewCity/Enum/EnumConditionType.cs
@@ -8,8 +8,15 @@ namespace NewCity.Enum
 
     public enum enumConditionType {大于=0,小于=1,等于=2,不等于=3 }
 
+    public enum enumEffectType { 增加 = 0, 减少 = 1, 赋值 = 2}
+
     public class EnumConditionType
     {
         public string[] enumConditionType = { ">", "<", "=", "≠" };
+    }
+
+    public class EnumEffectType
+    {
+        public string[] enumConditionType = { "增加", "减少", "赋值" };
     }
 }
