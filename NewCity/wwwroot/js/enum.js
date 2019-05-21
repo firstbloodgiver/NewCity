ac390c105f7e080a43cf0c3a09be58362c10407b
xu sen <632940702@qq.com>
2019-05-21 22:06:04 +0800

    change

--- NewCity/wwwroot/js/enum.js
+++ NewCity/wwwroot/js/enum.js
@@ -3,5 +3,13 @@ var enumConditionType = [">", "<", "=", "≠"];
 
 var enumEffectType = ["增加", "减少", "赋值"];
 
-const GuidEmpty = "00000000-0000-0000-0000-000000000000";
+const emptyguid = "00000000-0000-0000-0000-000000000000";
+
+//生成uuid
+function S4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+function guid() {
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
 
